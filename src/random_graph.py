5486dccce5274310e9d1d350a53cec5cc2c0ea8b
Layton Webber <wlayton@pdx.edu>
2020-05-10 12:03:26 -0700

    Optimized random graph generation, and removed the ability to introduce nonsense edges (self-edges, edges with source as target)

--- src/random_graph.py
+++ src/random_graph.py
@@ -1,8 +1,8 @@
 import random
 from ortools.graph import pywrapgraph
 
-NUM_NODES = 1000
-DENSITY = 0
+NUM_NODES = 500
+DENSE = True
 RANDOM_SEED = 5
 MAX_CAPACITY = 20
 
@@ -13,26 +13,35 @@ with open('../test_graphs/test_random.graph', 'w') as f:
     f.write(str(NUM_NODES) + '\n')
     f.write(str(source) + '\n')
     f.write(str(target) + '\n')
-    num_edges = random.randint(1,10)*NUM_NODES*((NUM_NODES*DENSITY) + 1)
+
+    all_edges = list()
+    for i in range(1, NUM_NODES):
+        for j in range(1, NUM_NODES):
+            if i != j and j != source and i != target:
+                all_edges.append((i,j))
+    num_edges = random.randint(NUM_NODES, len(all_edges))
+    if not DENSE:
+        num_edges = int(num_edges / NUM_NODES)
+
+    edges = random.sample(all_edges, num_edges)
+
     start_nodes = list([0]*num_edges)
     end_nodes = list([0]*num_edges)
     capacities = list([0]*num_edges)
     for i in range(num_edges):
-        s = random.randint(1, NUM_NODES)
-        e = random.randint(1, NUM_NODES)
-        while s == e:
-            s = random.randint(1, NUM_NODES)
-            e = random.randint(1, NUM_NODES)
+        s, e = edges[i]
         start_nodes[i] = s
         end_nodes[i] = e
-        capacities[i] = random.randint(1, NUM_NODES)
+        capacities[i] = random.randint(1, MAX_CAPACITY)
 
     max_flow = pywrapgraph.SimpleMaxFlow()
     for s, e, c in zip(start_nodes, end_nodes, capacities):
         max_flow.AddArcWithCapacity(s, e, c)
 
-    max_flow.Solve(source, target)
-    f.write(str(max_flow.OptimalFlow()) + '\n')
+    if max_flow.Solve(source, target) == max_flow.OPTIMAL:
+        f.write(str(max_flow.OptimalFlow()) + '\n')
+    else:
+        print("there was an issue")
 
     for s, e, c in zip(start_nodes, end_nodes, capacities):
         f.write(str(s) + ' ' + str(e) + ' ' + str(c) + '\n')
