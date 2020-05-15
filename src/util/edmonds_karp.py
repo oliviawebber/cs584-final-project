ea1779ba8bbce5ed9e74fa4dbd152548ae079a09
Layton Webber <wlayton@pdx.edu>
2020-05-15 15:04:03 -0700

    Added comments to explain steps in edmonds karp

--- src/util/edmonds_karp.py
+++ src/util/edmonds_karp.py
@@ -1,11 +1,15 @@
 from collections import deque
 from copy import copy
+# This class implements the Edmonds Karp algorithm for Max-Flow/Min-Cut. It is
+# based off the psuedo-code for this algorithm found in Introduction to Algorithms
+# 3rd Edition by Cormen, Leiserson, Rivest, Stein.
 
 class Edmonds_Karp:
     def __init__(self, g):
         self.g = g
         self.g_res = copy(g)
         sz = self.g_res.dim()
+        # Initialize backwards edges in the residual graph
         for i in range(1, sz + 1):
             for j in range(1, sz + 1):
                 forward_edge = self.g_res.get_edge(i, j)
@@ -18,6 +22,8 @@ class Edmonds_Karp:
         self.flow = 0
 
     def BFS(self):
+        # Performs breadth first search between the source and target nodes, in
+        # the residual graph
         v = self.g_res.get_source()
         target = self.g_res.get_target()
         parent = {v: None}
@@ -26,6 +32,8 @@ class Edmonds_Karp:
         while len(q) != 0:
             current_node = q.pop()
             if current_node == target:
+                # Create the path by tracing back through parents until we reach
+                # the source
                 p = []
                 while current_node != None:
                     p.append(current_node)
@@ -33,6 +41,7 @@ class Edmonds_Karp:
                 p.reverse()
                 return p
 
+            # Search along our outgoing edges for a neighbor
             for neighbor, capacity in self.g_res.get_out_neighbors(current_node):
                 if neighbor not in visited and capacity > 0:
                     parent[neighbor] = current_node
@@ -42,8 +51,12 @@ class Edmonds_Karp:
 
     def augment(self, p):
         pairs = [(p[i], p[i+1]) for i in range(len(p)-1)]
+        # delta is the maximum flow we can push along the augmenting path
+        # we have found
         delta = min([self.g_res.get_edge(p, q) for p, q in pairs])
         self.flow += delta
+        # for each edge in the augmenting path, push delta flow towards the
+        # target and delta flow back along the residual edges
         for p, q in pairs:
             new_capacity = self.g_res.get_edge(p, q) - delta
             self.g_res.add_edge(p, q, new_capacity)
@@ -53,6 +66,7 @@ class Edmonds_Karp:
     def max_flow(self):
         while True:
             p = self.BFS()
+            # If we can't find an augmenting path we're done
             if p is None:
                 break
             self.augment(p)
