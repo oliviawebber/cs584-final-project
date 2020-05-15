a14a9dad33880e6eb49ccad941e66f5a46cf22ac
Layton Webber <wlayton@pdx.edu>
2020-05-15 14:50:40 -0700

    Added explanation of data structures

--- src/util/data_structure.py
+++ src/util/data_structure.py
@@ -1,6 +1,10 @@
 from collections import deque
 from util.enums import Storage
+# This file defines wrapper classes around deque's for each of the possible
+# data structure types the Boykov-Kolmogorov algorithm can use
 
+# This serves as a base class for the data structures which only use one
+# underlying deque
 class SingleDataStructure:
     def __init__(self, iterable):
         self.ds = deque(iterable)
@@ -19,16 +23,20 @@ class SingleDataStructure:
     def __iter__(self):
         return iter(self.ds)
 
+# Wrapper so behavior is like a stack
 class Stack(SingleDataStructure):
     def __init__(self, iterable):
         super().__init__(iterable)
         self.add = self.ds.appendleft
 
+# Wrapper so behavior is like a queue
 class Queue(SingleDataStructure):
     def __init__(self, iterable):
         super().__init__(iterable)
         self.add = self.ds.append
 
+# This serves as a base call for the data structures which have two underlying
+# deque's. These deque's are switched whenever one of them becomes full
 class MultiDataStructure:
     def __init__(self, iterable):
         self.active_ds = deque(iterable)
@@ -56,13 +64,13 @@ class MultiDataStructure:
     def __iter__(self):
         return iter(self.active_ds + self.inactive_ds)
 
-
-
+# Wrapper so both deques behave like stacks
 class MultiStack(MultiDataStructure):
     def __init__(self, iterable):
         super().__init__(iterable)
         self.add = self.inactive_ds.appendleft
 
+# Wrapper so both deques behave like queues
 class MultiQueue(MultiDataStructure):
     def __init__(self, iterable):
         super().__init__(iterable)
