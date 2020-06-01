2ca40d9b3244d02c638904f6551ce4d8f93f1a6b
Layton Webber <wlayton@pdx.edu>
2020-06-01 09:36:14 -0700

    Expanded testing framework

--- src/main.py
+++ src/main.py
@@ -5,46 +5,42 @@ from util.goldberg_tarjan import Goldberg_Tarjan as gt
 from util.enums import Storage
 from timeit import timeit
 import os
+import shutil
 
 TESTS = 10
 data_structures = [Storage.queue, Storage.stack, Storage.multi_queue, Storage.multi_stack]
 parameters = [True, False]
 graph_path = "../test_graphs"
+complete_path = "../complete_tests"
+results = "../results/"
 
 for g in os.listdir(graph_path):
+    print("Working on graph %s" % g)
     test_graph = Graph.read_graph(graph_path + '/' + g)
-    with open('edmond_karp_results', 'a') as f:
+    print("Starting Edmond-Karp Test...", end=" ")
+    with open(results + 'edmond_karp_results', 'a') as f:
         ek_test_instance = ek(test_graph)
         f.write(g + ',')
-        f.write(timeit(ek_test_instance.max_flow, number = TESTS) / TESTS)
+        f.write(str(timeit(ek_test_instance.max_flow, number = TESTS) / TESTS))
         f.write('\n')
-    with open('goldberg_tarjan_results', 'a') as f:
+    print("done")
+    print("Starting Goldberg-Tarjan Test...", end=" ")
+    with open(results + 'goldberg_tarjan_results', 'a') as f:
         gt_test_instance = gt(test_graph)
         f.write(g + ',')
-        f.write(timeit(gt_test_instance.max_flow, number = TESTS) / TESTS)
+        f.write(str(timeit(gt_test_instance.max_flow, number = TESTS) / TESTS))
         f.write('\n')
+    print("done")
     for data_structure in data_structures:
         for store_parent_info in parameters:
             for perfect_info in parameters:
                 for store_child_info in parameters:
-                    with open('boykov_kolmogorov_%s_%s%s%s_results' % (data_structure, store_parent_info, perfect_info, store_child_info), 'a') as f:
+                    print("Starting Boykov-Kolmogorov (%s,%s,%s,%s) Test..." % (data_structure, store_parent_info, perfect_info, store_child_info), end=" ")
+                    with open(results + 'boykov_kolmogorov_%s_%s%s%s_results' % (data_structure, store_parent_info, perfect_info, store_child_info), 'a') as f:
                         bk_test_instance = bk(test_graph, data_structure, data_structure, store_parent_info, perfect_info, store_child_info)
                         f.write(g + ',')
-                        f.write(timeit(bk_test_instance.max_flow, number = TESTS) / TESTS)
+                        f.write(str(timeit(bk_test_instance.max_flow, number = TESTS) / TESTS))
                         f.write('\n')
-
-
-
-#my_graph = Graph.read_graph("../test_graphs/test0-c50-n300-e72120.graph")
-#my_graph = Graph.read_graph("../test_graphs/test1.graph")
-#my_bk = bk(my_graph, Storage.multi_queue, Storage.multi_queue, False, False, False)
-#my_ek = ek(my_graph)
-#my_gt = gt(my_graph)
-#print(my_bk.max_flow())
-#print("BK time: " + str(timeit(my_bk.max_flow, number = TESTS) / TESTS))
-#print("EK time: " + str(timeit(my_ek.max_flow, number = 1000) / 1000))
-
-#print("GT time: " + str(timeit(my_gt.max_flow, number = TESTS) / TESTS))
-
-#my_gt.max_flow()
-#my_bk.max_flow()
+                    print("done")
+    print("Done with graph %s" % g)
+    shutil.move(graph_path + '/' + g, complete_path + '/' + g)
